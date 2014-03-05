5611bce10b9871a1d808bfb4114650c6fa5d4fd8
Dylan Jones <monetizedesign@gmail.com>
2014-03-04 22:25:42 -0500

    added accessor :image_remote_url to user model

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,5 @@
 class Pin < ActiveRecord::Base
-	attr_accessible :asset_file_name
+	attr_accessible :description, :image, :image_remote_url
 	belongs_to :user
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 end
